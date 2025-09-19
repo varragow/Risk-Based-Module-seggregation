dd33910529ef2de25b80a5f40cccae8dac011cab
Varra Gowtham Reddy <varragow@qti.qualcomm.com>
2025-09-19 13:13:13 +0530

    Added requirements and setup.bat files

---
+++ setup.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Installing dependencies...
+
+pip install dash
+pip install pandas
+pip install plotly
+pip install scikit-learn
+pip install matplotlib
+pip install kneed
+pip install openpyxl
+pip install requests
+pip install beautifulsoup4
+pip install cryptography
+pip install dash-bootstrap-components
+echo Dependencies installed successfully.
+pause
